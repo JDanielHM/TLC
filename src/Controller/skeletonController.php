9bd96b6b6a6a1b2c818a3f8c37f451456ccc4bea
JDanielHM <hernandez27648@gmail.com>
2022-08-31 13:06:51 -0500

    first commit

---
+++ src/Controller/skeletonController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class skeletonController extends AbstractController
+{
+    /**
+     * @Route("/example", name="skelleton_example", methods={"GET"})
+     */
+    public function index()
+    {
+        return new JsonResponse([
+            'data' => 'success'
+        ]);
+    }
+}
